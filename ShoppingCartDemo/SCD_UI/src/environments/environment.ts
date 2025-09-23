2872353769e65754fd104dbb6dce1fd235ac70bc
rajeevsharma4nagarro <rajeev.sharma@nagarro.com>
2025-09-23 12:45:10 +0530

    Update environment.ts

--- ShoppingCartDemo/SCD_UI/src/environments/environment.ts
+++ ShoppingCartDemo/SCD_UI/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   authApiUrl: 'http://localhost:7002/api',
+  //authApiUrl: 'http://localhost:7000/api',
   productApiUrl: 'http://localhost:7000/api',
   cartApiUrl: 'http://localhost:7000/api',
   orderApiUrl: 'http://localhost:7000/api'
