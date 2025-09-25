0b3a22ee62897ab18365c9c58765351d9ea3952f
Rajeev <rajeev.sharma@nagarro.com>
2025-09-26 00:16:11 +0530

    environment variable changes

--- ShoppingCartDemo/SCD_UI/src/environments/environment.prod.ts
+++ ShoppingCartDemo/SCD_UI/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  authApiUrl: 'http://localhost:7002/api',
-  productApiUrl: 'http://localhost:7000/api',
+  authApiUrl: 'https://scd-as-auth-f9h3btb6e8gfbuah.centralindia-01.azurewebsites.net/api', 
+  productApiUrl: 'scd-as-product-gbe6dkaabjffeafk.centralindia-01.azurewebsites.net/api',
   cartApiUrl: 'http://localhost:7000/api',
   orderApiUrl: 'http://localhost:7000/api'
 };
