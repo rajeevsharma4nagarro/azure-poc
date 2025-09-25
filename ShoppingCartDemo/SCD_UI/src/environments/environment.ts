324cad47d58853dc7a394879f12d4efd506eae4b
Rajeev <rajeev.sharma@nagarro.com>
2025-09-25 15:31:13 +0530

    deployment changes

--- ShoppingCartDemo/SCD_UI/src/environments/environment.ts
+++ ShoppingCartDemo/SCD_UI/src/environments/environment.ts
@@ -1,10 +1,24 @@
 export const environment = {
   production: false,
-  authApiUrl: 'http://localhost:7002/api',
 
-  //productApiUrl: 'http://localhost:7003/api',
-  //cartApiUrl: 'http://localhost:7004/api',
-  //orderApiUrl: 'http://localhost:7005/api'
+  
+  /*local without API gateway
+
+  authApiUrl: 'http://localhost:7002/api',
+  productApiUrl: 'http://localhost:7003/api',
+  cartApiUrl: 'http://localhost:7004/api',
+  orderApiUrl: 'http://localhost:7005/api'
+  */
+
+  /*local with api gateway
+  
+  authApiUrl: 'http://localhost:7002/api',
+  productApiUrl: 'http://localhost:7000/api',
+  cartApiUrl: 'http://localhost:7000/api',
+  orderApiUrl: 'http://localhost:7000/api'
+  */
+
+  authApiUrl: 'https://scd-as-auth-f9h3btb6e8gfbuah.centralindia-01.azurewebsites.net/api', 
   productApiUrl: 'http://localhost:7000/api',
   cartApiUrl: 'http://localhost:7000/api',
   orderApiUrl: 'http://localhost:7000/api'
