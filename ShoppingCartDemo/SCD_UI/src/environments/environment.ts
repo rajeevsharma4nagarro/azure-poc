ad6c3a668a5bdc824f4da38a9200a008fe7484a1
Rajeev <rajeev.sharma@nagarro.com>
2025-09-30 14:01:20 +0530

    testing

--- ShoppingCartDemo/SCD_UI/src/environments/environment.ts
+++ ShoppingCartDemo/SCD_UI/src/environments/environment.ts
@@ -4,10 +4,15 @@ export const environment = {
   
   /*local without API gateway  */
 
-  authApiUrl: 'http://localhost:7002/api',
-  productApiUrl: 'http://localhost:7003/api',
-  cartApiUrl: 'http://localhost:7004/api',
-  orderApiUrl: 'http://localhost:7005/api'
+  // authApiUrl: 'http://localhost:7002/api',
+  // productApiUrl: 'http://localhost:7003/api',
+  // cartApiUrl: 'http://localhost:7004/api',
+  // orderApiUrl: 'http://localhost:7005/api'
+
+  authApiUrl: 'https://scd-as-auth-f9h3btb6e8gfbuah.centralindia-01.azurewebsites.net/api', 
+  productApiUrl: 'https://scd-as-product-gbe6dkaabjffeafk.centralindia-01.azurewebsites.net/api',
+  cartApiUrl: 'https://scd-as-cart-bwdfc9ajazhddnfd.centralindia-01.azurewebsites.net/api',
+  orderApiUrl: 'https://scd-as-order-b7cwbtdkg2hbcxee.centralindia-01.azurewebsites.net/api'
 
 
   /*local with api gateway 
@@ -17,4 +22,6 @@ export const environment = {
   cartApiUrl: 'http://localhost:7000/api',
   orderApiUrl: 'http://localhost:7000/api'
  */
+
+  /*angular UI demo for code sync 3  */
 };
